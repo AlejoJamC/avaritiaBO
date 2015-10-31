a6e0992d9f5952807d94d26376d130415f571a94
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-10-30 20:58:41 -0500

    routes and router, minor image

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+var express = require('express');
+var routerIndex = express.Router();
+var request = require('request');
+
+/* GET home page. */
+routerIndex.get('/', function (req, res) {
+    res.render('index', {
+        title : 'Avaritia | Interfaz administrativa',
+        level : ''
+    });
+});
+
+module.exports = routerIndex;
