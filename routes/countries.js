d59cac7049cdaccda6834e307363b10261229019
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-22 03:43:29 -0500

    country route

--- routes/countries.js
+++ routes/countries.js
@@ -9,12 +9,56 @@
 var express = require('express');
 var countryRoutes = express.Router();
 var request = require('request');
+var S = require('string');
 
 /* GET home page. */
 countryRoutes.get('/paises', function (req, res) {
-    res.render('countries', {
-        title : 'Paises | Avaritia',
-        level : ''
+
+    // Get the list of all countries
+    var options = {
+        'url': global.WEBSERVICE + '/countries',
+        'auth':{
+            'bearer': global.BEARERTOKEN
+        }
+    };
+
+    request.get(options, function (error, response, body) {
+        var countries = [];
+        // TODO: arreglar el scope de estas variables en la obtencion de datos de los paises
+        var processMessage='';
+        var errorState = false;
+        var fakeId;
+
+        if(!error && response.statusCode == 200){
+            var info = JSON.parse(body);
+            for (var i = 0; i < info.length; i++){
+                fakeId= i++;
+                countries[i] = {
+                    id: fakeId,
+                    hiddenId: info[i]._id,
+                    name: info[i].name,
+                    creationDate: info[i].creationDate,
+                    lastEditionDate: info[i].lastEditionDate,
+                    enabled: info[i].enabled
+                }
+            }
+            processMessage = 'Lista completa cargada sin problemas';
+            errorState = false;
+        }else{
+            processMessage = 'Error: obteniendo la lista de los paises';
+            errorState = true;
+        }
+        console.log(countries);
+        res.render('countries', {
+            title : 'Paises | Avaritia',
+            level :  '',
+            processMessage: processMessage,
+            error: errorState,
+            module: 'Paises',
+            moduleURL:'paises',
+            moduleDescription:'Lista de paises habilitados',
+            countries : countries
+        });
     });
 });
 
